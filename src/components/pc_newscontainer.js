c3269d217fd1b2b9eac79b58d76f48062f89a3f3
inks0815 <252684401@qq.com>
2017-07-02 22:07:37 +0800

    图片

--- src/components/pc_newscontainer.js
+++ src/components/pc_newscontainer.js
@@ -5,6 +5,8 @@ import {Tabs,Carousel} from 'antd';
 
 import styles from '../css/pc.css';
 import PCNewsBlock from './pc_news_block';
+import PCNewsImageBlock from './pc_news_image_block';
+
 const TabPane =Tabs.TabPane;
 
 export default class PCNewsContainer extends React.Component {
@@ -32,12 +34,21 @@ export default class PCNewsContainer extends React.Component {
                        <div><h3>4</h3></div>
                      </Carousel>
                   </div>
+                  <PCNewsImageBlock count={6} type="guoji" width="400px" cartTitle="国际头条" imageWidth="112px"/>
              </div>
+
              <Tabs class="tabs_news">
                   <TabPane tab="新闻" key="1">
                           <PCNewsBlock count={22} type="top" width="100%" bordered="false"/>
                   </TabPane>
+                  <TabPane tab="娱乐" key="2">
+                          <PCNewsBlock count={22} type="top" width="100%" bordered="false"/>
+                  </TabPane>
              </Tabs>
+             <div>
+               <PCNewsImageBlock count={8} type="guonei" width="100%" cartTitle="国内新闻" imageWidth="132px"/>
+               <PCNewsImageBlock count={16} type="yule" width="100%" cartTitle="娱乐新闻" imageWidth="132px"/>
+             </div>
           <Col span={2}> </Col>
      </Row>
   </div>);
